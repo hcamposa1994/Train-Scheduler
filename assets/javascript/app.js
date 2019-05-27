ee2ca5916e931cddd85986afe4914c62d1e7beca
Herbert Campos <wohoforever@yahoo.com>
2019-05-26 17:18:11 -0700

    html set up plus firebase stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBWrczxpuv4SEKypPNlxaMljOgjD-AQ51w",
+    authDomain: "train-scheduler-4f343.firebaseapp.com",
+    databaseURL: "https://train-scheduler-4f343.firebaseio.com",
+    projectId: "train-scheduler-4f343",
+    storageBucket: "train-scheduler-4f343.appspot.com",
+    messagingSenderId: "468499257399",
+    appId: "1:468499257399:web:d6aa8d770f38932c"
+  };
+  firebase.initializeApp(firebaseConfig);
