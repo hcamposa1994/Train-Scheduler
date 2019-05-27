19818666620d37c83d7c25290f5e601ab9e1a620
Herbert Campos <wohoforever@yahoo.com>
2019-05-26 20:57:20 -0700

    first draft

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,4 +7,70 @@ var firebaseConfig = {
     messagingSenderId: "468499257399",
     appId: "1:468499257399:web:d6aa8d770f38932c"
   };
-  firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
+$("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+
+    var trainName = $("#train-name-input").val().trim();
+    var trainDest = $("#destination-input").val().trim();
+    var trainTime = $("#time-input").val().trim();
+    var trainFreq = $("#frequency-input").val().trim();
+
+    var newtrain = {
+        name: trainName,
+        destination: trainDest,
+        time: trainTime,
+        frequency: trainFreq
+    };
+
+    database.ref().push(newtrain);
+
+    alert("Train schedule successfully added");
+
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#time-input").val("");
+    $("#frequency-input").val("");
+});
+
+database.ref().on("child_added", function(snapshot) {
+    console.log(snapshot.val());
+
+    // Store everything into a variable.
+    var trainName = snapshot.val().name;
+    var trainDest = snapshot.val().destination;
+    var trainTime = snapshot.val().time;
+    var trainFreq = snapshot.val().frequency;
+
+    // Employee Info
+    console.log(trainName);
+    console.log(trainDest);
+    console.log(trainTime);
+    console.log(trainFreq);
+
+    var firstTimeConverted = moment(firstTrainTime, "hh:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    var tRemainder = diffTime % frequency;
+    var minutesTillTrain = frequency - tRemainder;
+    var nextTrain = moment().add(minutesTillTrain, "minutes");
+    var nextTrainFormatted = moment(nextTrain).format("hh:mm");
+
+    // Create the new row
+    var newRow = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(trainDest),
+        $("<td>").text(trainFreq),
+        $("<td>").text(nextTrainFormatted),
+        $("<td>").text(minutesTillTrain)
+    );
+
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+
+// Handle the errors
+}, function(errorObject) {
+console.log("Errors handled: " + errorObject.code);
+});
